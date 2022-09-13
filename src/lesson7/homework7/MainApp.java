6ec74f0334bf9a6e12f3c35525c0fd2d887c38d0
megav01t <simpilyator@gmail.com>
2022-09-13 17:54:00 +0300

    Выполнила ДЗ7

---
+++ src/lesson7/homework7/MainApp.java
@@ -0,0 +1,11 @@
+package lesson7.homework7;
+
+import java.io.IOException;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        UserInterface userInterface = new UserInterface();
+        userInterface.runApplication();
+    }
+}
